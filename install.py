e9270bdfd45e5b32cf3f8e7ece0ade882fcb202f
patrinhani <patrinhani.gui@gmail.com>
2025-03-27 09:24:50 -0300

    Adds 'selenium' package to the list of packages to install
    Updates the 'packages' list in the 'install.py' file to include the 'selenium' package, ensuring it is installed or updated along with other packages.
    
    Fixes #3

--- install.py
+++ install.py
@@ -9,7 +9,7 @@ def install_or_update_package(package):
         print(f"❌ Erro ao instalar/atualizar {package}")
 
 # Lista de pacotes para instalar
-packages = ["numpy", "scikit-learn", "openai", "ttkbootstrap", "fitz"]
+packages = ["numpy", "scikit-learn", "openai", "ttkbootstrap", "fitz", "selenium"]
 
 for package in packages:
     install_or_update_package(package)
